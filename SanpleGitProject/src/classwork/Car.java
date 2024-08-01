8eaa7ae25fa2711a716a66c46c5dfc62fdc312a8
yashh <yashh@ShuklaThinkpad.lan>
2024-08-01 16:21:07 -0400

    saving project to github

---
+++ SanpleGitProject/src/classwork/Car.java
@@ -0,0 +1,17 @@
+package classwork;
+
+public class Car {
+	
+	public void drive() {
+		System.out.println("In driver");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("my car is red color");
+		
+
+	}
+
+}
---
+++ SanpleGitProject/src/classwork/HelloWord.java
@@ -0,0 +1,11 @@
+package classwork;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		System.out.println("this is a my word");
+		
+
+	}
+
+}
