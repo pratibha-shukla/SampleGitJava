4d00d4717650b44d04d1cae89280e00d83553706
yashh <yashh@ShuklaThinkpad.lan>
2024-08-01 16:35:46 -0400

    saving new changes

--- SanpleGitProject/src/classwork/Car.java
+++ SanpleGitProject/src/classwork/Car.java
@@ -4,6 +4,7 @@ public class Car {
 	
 	public void drive() {
 		System.out.println("In driver");
+		System.out.println("that is your blue car");
 		
 	}
 
--- SanpleGitProject/src/classwork/HelloWord.java
+++ SanpleGitProject/src/classwork/HelloWord.java
@@ -4,6 +4,7 @@ public class HelloWord {
 
 	public static void main(String[] args) {
 		System.out.println("this is a my word");
+		System.out.println("that is my personal git hub");
 		
 
 	}
