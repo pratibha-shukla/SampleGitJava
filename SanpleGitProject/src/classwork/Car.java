022f330c657e73d5fb51ef4ff633fbdbd4dfb9e7
pratibha-shukla <pratibha.email@gmail.com>
2024-08-02 11:13:17 -0400

    Update Car.java

--- SanpleGitProject/src/classwork/Car.java
+++ SanpleGitProject/src/classwork/Car.java
@@ -5,7 +5,7 @@ public class Car {
 	public void drive() {
 		System.out.println("In driver");
 		System.out.println("that is your blue car");
-		
+		System.out.println("my new update by Babli")
 	}
 
 	public static void main(String[] args) {
