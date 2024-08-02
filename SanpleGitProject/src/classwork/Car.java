c7579245e2a4ced8f01415414ddad50d327ba6eb
pratibha-shukla <pratibha.email@gmail.com>
2024-08-02 11:21:01 -0400

    Update Car.java

--- SanpleGitProject/src/classwork/Car.java
+++ SanpleGitProject/src/classwork/Car.java
@@ -5,12 +5,18 @@ public class Car {
 	public void drive() {
 		System.out.println("In driver");
 		System.out.println("that is your blue car");
-		System.out.println("my new update by Babli")
+		System.out.println("my new update by Babli");
+		System.out.println("my new method in my home")
 	}
+	public void rmethos(){
+			System.out.println("my truck is blue color");
 
 	public static void main(String[] args) {
 		
 		System.out.println("my car is red color");
+
+		
+			
 		
 
 	}
