47906b1ea8840fc81208391d1276df80e13c3c01
pratibha-shukla <pratibha.email@gmail.com>
2024-08-07 20:22:01 -0400

    some new

--- SanpleGitProject/src/classwork/HelloWord.java
+++ SanpleGitProject/src/classwork/HelloWord.java
@@ -6,6 +6,7 @@ public class HelloWord {
 		System.out.println("this is a my word");
 		System.out.println("that is my personal git hub");
 		System.out.println("August is bet month in a year");
+		System.out.println("August is best month in a year");
 		
 		
 
