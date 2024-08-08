14d183132e74ece373ee54e50d53e25ffd5c7457
pratibha-shukla <pratibha.email@gmail.com>
2024-08-07 20:17:55 -0400

    new changes in hello world

--- SanpleGitProject/src/classwork/HelloWord.java
+++ SanpleGitProject/src/classwork/HelloWord.java
@@ -5,6 +5,8 @@ public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("this is a my word");
 		System.out.println("that is my personal git hub");
+		System.out.println("August is bet month in a year");
+		
 		
 
 	}
